fac9e560a3de1cbebf4f91f3643c8a177e2dc9ee
Diogo Veloso <diogo.costa.veloso@gmail.com>
2023-01-25 19:24:00 +0000

    Update

--- Assets/Scripts/CarScripts/Car_Controller.cs
+++ Assets/Scripts/CarScripts/Car_Controller.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Car_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody sphereRB;
+    private float moveInput;
+    private float turnInput;
+
+    public float fwdSpeed;
+    public float backSpeed;
+    public float turnSpeed;
+
+    private void Start()
     {
+        sphereRB.transform.parent = null;
+    }
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
         
+        moveInput *= moveInput > 0 ? fwdSpeed : backSpeed;
+
+    
+        transform.position = sphereRB.transform.position;
+
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+
+        transform.Rotate(0, newRotation, 0,Space.World);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        sphereRB.AddForce(transform.forward * moveInput,ForceMode.Acceleration);
     }
 }
